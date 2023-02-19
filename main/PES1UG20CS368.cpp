22df0ffc1ed3e6d70e65b6cca227908a69b972b6
SaiSamarth <95733415+saisamarthtaluri@users.noreply.github.com>
2023-02-19 22:35:36 +0530

    Delete PES1UG20CS368.cpp

--- main/PES1UG20CS368.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    cout<<"Hello World\n";
-    cout<<"New .cpp file to test for task 5.\n";
-    cout<<"By SaiSamarth PES1UG20CS368\n";
-    return 0;
-}
